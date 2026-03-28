db345f35c5ec8323cddfe29583aefd3092ba646f
Your Name <you@example.com>
2026-03-28 15:26:39 +0300

    hola

---
+++ team00_ex01.sql
@@ -0,0 +1,44 @@
+CREATE TABLE
+roads (point1 VARCHAR,
+       point2 VARCHAR,
+       cost INT);
+
+INSERT INTO roads (point1, point2, cost)
+VALUES ('a', 'b', 10), ('b', 'a', 10),
+       ('b', 'c', 35), ('c', 'b', 35),
+       ('a', 'c', 15), ('c', 'a', 15),
+       ('a', 'd', 20), ('d', 'a', 20),
+       ('b', 'd', 25), ('d', 'b', 25),
+       ('c', 'd', 30), ('d', 'c', 30);
+
+WITH RECURSIVE
+paths AS (
+    SELECT ARRAY[roads.point1, roads.point2] AS tour,
+           roads.point1,
+           roads.point2,
+           roads.cost,
+           roads.cost AS summ
+    FROM roads
+    WHERE roads.point1 = 'a'
+
+    UNION ALL
+
+    SELECT ARRAY_APPEND(paths.tour, roads.point2) AS tour,
+           roads.point1,
+           roads.point2,
+           roads.cost,
+           paths.summ + roads.cost AS summ
+    FROM paths JOIN roads ON paths.point2 = roads.point1
+    WHERE ARRAY_POSITION(paths.tour, roads.point2) IS NULL),
+
+valid_tours AS (
+    SELECT paths.summ + roads.cost AS total_cost,
+           ARRAY_APPEND(paths.tour, 'a') AS tour
+    FROM paths
+    JOIN roads ON paths.point2 = roads.point1 AND roads.point2 = 'a'
+    WHERE cardinality(paths.tour) = 4)
+
+SELECT *
+FROM valid_tours
+WHERE total_cost = (SELECT MIN(total_cost) FROM valid_tours)
+ORDER BY total_cost, tour;
